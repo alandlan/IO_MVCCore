fb4cc8d5a7797d36c6c966a29a075365a85d8cf2
Alan Vieira Martins <alanhorcrux@gmail.com>
2019-11-16 08:59:17 -0300

    Implementando áreas

--- AppModelo/src/DevIO.UI.Site/Startup.cs
+++ AppModelo/src/DevIO.UI.Site/Startup.cs
@@ -34,6 +34,10 @@ namespace DevIO.UI.Site
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
