9ab928a34767ac67805839a9e7d34c60344cfae2
Alan Vieira Martins <alanhorcrux@gmail.com>
2019-11-17 21:06:24 -0300

    Usando Transient, Scoped e Singleton

--- AppModelo/src/DevIO.UI.Site/Controllers/HomeController.cs
+++ AppModelo/src/DevIO.UI.Site/Controllers/HomeController.cs
@@ -1,24 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevIO.UI.Site.Data;
+using DevIO.UI.Site.Servicos;
+using Microsoft.AspNetCore.Mvc;
 using static DevIO.UI.Site.Data.PedidoRepository;
 
 namespace DevIO.UI.Site.Controllers
 {
     public class HomeController : Controller
     {
-        //private readonly IPedidoRepository _pedidoRepository;
-        //public HomeController(IPedidoRepository pedidoRepository)
-        //{
-        //    _pedidoRepository = pedidoRepository;
-        //}
+        private readonly IPedidoRepository _pedidoRepository;
+
+        public HomeController(IPedidoRepository pedidoRepository)
+        {
+            _pedidoRepository = pedidoRepository;
+        }
+
         public IActionResult Index([FromServices] IPedidoRepository _pedidoRepository)
         {
             var pedido = _pedidoRepository.ObterPedido();
 
-            return View(pedido);
+            return View();
         }
     }
 }
